0413c65baf1ea97a6fe6d1cb3ae902c50ce40052
vulm-neo01 <minhvu8a@gmail.com>
2023-02-01 08:22:09 +0700

    Update admin

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -2,8 +2,6 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.scss";
 import Widget from "../../components/widget/Widget";
-import Featured from "../../components/featured/Featured";
-import Chart from "../../components/chart/Chart";
 import Table from "../../components/table/Table";
 
 const Home = () => {
@@ -11,21 +9,21 @@ const Home = () => {
     <div className="home">
       <Sidebar />
       <div className="homeContainer">
-        <Navbar />
-        <div className="widgets">
+        {/* <Navbar /> */}
+        {/* <div className="widgets">
           <Widget type="user" />
           <Widget type="order" />
           <Widget type="earning" />
           <Widget type="balance" />
-        </div>
-        <div className="charts">
+        </div> */}
+        {/* <div className="charts">
           <Featured />
           <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
-        </div>
-        <div className="listContainer">
+        </div> */}
+        {/* <div className="listContainer">
           <div className="listTitle">Latest Transactions</div>
           <Table />
-        </div>
+        </div> */}
       </div>
     </div>
   );
